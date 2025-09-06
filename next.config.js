233473ac65124d43728b3a7bd25b0b93cf538dd4
Peter Steinberger <steipete@gmail.com>
2025-09-06 11:42:05 +0100

    fix: resolve Next.js workspace root warning
    Add outputFileTracingRoot configuration to eliminate workspace detection warning about multiple lockfiles.

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ import "./src/env.js";
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  
+  // Fix workspace root warning
+  outputFileTracingRoot: ".",
 
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
